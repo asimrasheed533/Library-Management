197f50abd4815129a36c47a9905db714143e0283
Asim Rasheed <asimrasheed442@gmail.com>
2025-05-28 07:39:46 +0500

    Update page.tsx

--- app/(customer)/page.tsx
+++ app/(customer)/page.tsx
@@ -72,6 +72,109 @@ export default function Home() {
           />
         ))}
       </div>
+      {/* what will you learn */}
+      <div className="learn__container">
+        <div className="learn__container__text__warper">
+          <div className="learn__container__text__heading">
+            What will you learn?
+          </div>
+          <div className="learn__container__text__subheading">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod
+            corporis iusto reprehenderit earum perspiciatis at tempora
+            distinctio voluptatum deserunt ad natus adipisci delectus pariatur,
+            facere porro quae laudantium quisquam dolorem optio totam vitae
+            saepe, deleniti perferendis! Ducimus eum laudantium alias
+            voluptates, possimus nam inventore ea voluptas dignissimos facilis
+            aperiam earum.quae laudantium quisquam dolorem optio totam vitae
+            saepe, deleniti perferendis! Ducimus eum laudantium alias
+            voluptates, possimus nam inventore ea voluptas dignissimos facilis
+            aperiam earum.
+          </div>
+        </div>
+        <div className="learn__points__warper">
+          <div className="learn__points__entry">
+            <div className="learn__points__entry__number">1</div>
+            <div className="learn__points__entry__text">
+              <div className="learn__points__entry__text__title">
+                This typical journey into product management
+              </div>
+              <div className="learn__points__entry__text__subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                blanditiis id ducimus, recusandae ea soluta. adipisicing elit.
+                Esse blanditiis id ducimus, recusandae ea soluta.
+              </div>
+            </div>
+          </div>
+          <div className="learn__points__entry">
+            <div className="learn__points__entry__number">1</div>
+            <div className="learn__points__entry__text">
+              <div className="learn__points__entry__text__title">
+                This typical journey into product management
+              </div>
+              <div className="learn__points__entry__text__subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                blanditiis id ducimus, recusandae ea soluta. adipisicing elit.
+                Esse blanditiis id ducimus, recusandae ea soluta.
+              </div>
+            </div>
+          </div>
+          <div className="learn__points__entry">
+            <div className="learn__points__entry__number">1</div>
+            <div className="learn__points__entry__text">
+              <div className="learn__points__entry__text__title">
+                This typical journey into product management
+              </div>
+              <div className="learn__points__entry__text__subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                blanditiis id ducimus, recusandae ea soluta. adipisicing elit.
+                Esse blanditiis id ducimus, recusandae ea soluta.
+              </div>
+            </div>
+          </div>
+          <div className="learn__points__entry">
+            <div className="learn__points__entry__number">1</div>
+            <div className="learn__points__entry__text">
+              <div className="learn__points__entry__text__title">
+                This typical journey into product management
+              </div>
+              <div className="learn__points__entry__text__subtitle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse
+                blanditiis id ducimus, recusandae ea soluta. adipisicing elit.
+                Esse blanditiis id ducimus, recusandae ea soluta.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* chapter brekdown */}
+      <div className="breakdown__container">
+        <div className="breakdown__title__warper">
+          <div className="breakdown__title">
+            How to Product Chapter BreakDown
+          </div>
+          <div className="breakdown__subtitle">
+            Short discription about each chapter, Have any question? Contact me
+          </div>
+        </div>
+        <div className="breakdown__detail__warper">
+          <div className="breakdown__detail__text">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Necessitatibus, consequatur alias animi obcaecati, dolorem expedita
+            placeat quis quisquam maxime optio sint, quidem exercitationem.
+            Neque, dignissimos. Placeat ratione esse vel tempora amet quasi,
+            omnis qui ab debitis eos obcaecati saepe. Cupiditate dolor ipsum
+            nemo odio sint officia ipsam neque voluptas, harum voluptatem,
+            distinctio esse voluptatum excepturi nam suscipit vel incidunt
+            quasi!
+          </div>
+          <div className="breakdown__detail__image">
+            <img
+              src="https://res.cloudinary.com/dsxbqyjwo/image/upload/v1746016072/2575_vjen3c.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
